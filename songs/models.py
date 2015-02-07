68ed3f47705324efe77647ae5fd1a250fed2aca1
Colin  Wee <2gbleh@gmail.com>
2015-02-07 15:54:07 -0500

    resolved conflict also added __cmp__ method to Song()

--- songs/models.py
+++ songs/models.py
@@ -50,4 +50,20 @@ class Song(models.Model):
 	songName = models.CharField(max_length = 200)
 	songUrl = models.CharField(max_length = 250)
 	playlistID = models.IntegerField(default = 1)
-	playlistPosition = models.IntegerField(default = 1)
+    playlistPosition = models.IntegerField(default = 1)
+    
+    def __cmp__(self, notSelf):
+        return cmp(self.playlistPosition, notSelf.playlistPosition)
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    currentPlaylist = models.IntegerField(default = 1)
+    currentPosition = models.IntegerField(default = 1)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
+>>>>>>> Stashed changes
