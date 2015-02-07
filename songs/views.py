76d20aa86baeb6914dff9dc67103e69d76e64e79
Colin  Wee <2gbleh@gmail.com>
2015-02-07 03:47:19 -0500

    Link song template with index view
    added code to index()

--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,19 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader as templateLoader, RequestContext
+from django.shortcuts import render
 
-# Create your views here.
+from songs.models import Song
+
 def index(request):
-	return HttpResponse("Hello, world. You're at the songs index.")
+    """List all songs and their info"""
+    song_list = Song.objects
+    template = templateLoader.get_template("songs/index.html")
+    context = RequestContext(request, {
+        "song_list" : song_list,
+    })
+    return render(request, "polls/index.html", context)
 
 def playlist(request, playlist_id):
 	return HttpResponse("You're looking at the songs in playlist %s." % playlist_id)
