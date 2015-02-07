480e40968e3a57b4744eb9b39c3a4bb13b325803
Vivek Sridhar <vivek4830@gmail.com>
2015-02-06 23:35:20 -0500

    reset to default

--- GroupVibrations/urls.py
+++ GroupVibrations/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # http://www.youtube.com/watch?v=oHg5SJYRHA0
-    # url(r'^$', 'GroupVibrations.views.home', name='home'),
+    # url(r'^$', 'testsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
