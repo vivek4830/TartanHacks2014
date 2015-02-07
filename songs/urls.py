b983ac6553574d9c53d370619ddf52cf132cae8c
Vivek Sridhar <vivek4830@gmail.com>
2015-02-07 03:04:06 -0500

    Added playlist view framework

--- songs/urls.py
+++ songs/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 from songs import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name = 'index'))
+	url(r'^$', views.index, name = 'index'),
+	url(r'^(?P<playlist_id>\d+)/$', views.playlist, name = 'playlist'),
+)
--- songs/views.py
+++ songs/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, world. You're at the songs index.")
+
+def playlist(request, playlist_id):
+	return HttpResponse("You're looking at the songs in playlist %s." % playlist_id)
