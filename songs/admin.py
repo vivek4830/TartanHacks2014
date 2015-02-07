a22d375a4c3e8dcc016ae7f0b0979dd1e143d48d
Vivek Sridhar <vivek4830@gmail.com>
2015-02-07 04:04:57 -0500

    Updated Admin Page

--- songs/admin.py
+++ songs/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from songs.models import Song
+from songs.models import Song, Playlist
+
 # Register your models here.
-admin.site.register(Song)
+
+class SongAdmin(admin.ModelAdmin):
+	list_display = ('songName', 'songUrl', 'playlistID', 'playlistPosition')
+
+class PlaylistAdmin(admin.ModelAdmin):
+	list_display = ('playlistID', 'playlistName')
+
+admin.site.register(Song, SongAdmin)
+admin.site.register(Playlist, PlaylistAdmin)
--- songs/models.py
+++ songs/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+class Playlist(models.Model):
+	playlistID = models.IntegerField(default = 1)
+	playlistName = models.CharField(max_length = 200)
+
 # Create your models here.
 class Song(models.Model):
+	playlist = models.ForeignKey(Playlist, default = 1)
 	songName = models.CharField(max_length = 200)
 	songUrl = models.CharField(max_length = 250)
-	playlistID = models.IntegerField(default = -1)
-	playlistPosition = models.IntegerField(default = -1)
+	playlistID = models.IntegerField(default = 1)
+	playlistPosition = models.IntegerField(default = 1)
